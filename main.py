894e5a998102b4aaf8f088e0e4d94057daed2260
bruno victor <brunorobbz@gmail.com>
2026-04-20 17:02:30 -0300

    feat: Adicionando funcionalidades

--- main.py
+++ main.py
@@ -1,37 +1,52 @@
 import pandas as pd
-def extract():
-    ids = pd.read_csv("data/ids.csv")
-    users = pd.read_csv("data/users.csv")
-    return ids, users
+import sys
 
-def transform(ids, users):
-    if len(ids) != len(users):
-     raise ValueError("Invalid data: mismatch in number of IDs and users.")
+def extract(caminho):
+    if (caminho.endswith(".csv")):
+     users = pd.read_csv(caminho)    
+    elif (caminho.endswith("json")):
+     users = pd.read_json(caminho)
+    else:
+     raise ValueError(f"Formato de arquivo '{caminho}' não suportado. Use um arquivo CSV ou JSON.")
+    return users
+
+def transform(users):
     users = users.copy()
-    users["user_id"] = ids["UserID"].values
+    users["user_id"] = range(1, len(users) + 1)
     cols = ["user_id"] + [col for col in users.columns if col != "user_id"]
     users = users[cols]
     users = users.dropna(subset=["user_age"])
-    users = users[users["user_age"] >= 18].copy()
     users["user_age"] = users["user_age"].astype(int)
     users["user_wage"] = users["user_wage"].astype(float)
     users["user_name"] = users["user_name"].str.strip().str.title()
     users["bonus"] = users["user_wage"] * 0.1
     return users
 
-def load(df):
-    df.to_json("output/processed_users.json", orient="records", indent=4)
-    print(f"{len(df)} registros salvos com sucesso.")
+def load(df, formato="json"):
+    if formato.lower() == "json":
+     caminho_saida = "output/processed_users.json"
+     df.to_json(caminho_saida, orient="records", indent=4)
+     print(f"{len(df)} registros salvos com sucesso.\nArquivo JSON gerado na pasta Output em {caminho_saida}.")
+    elif formato.lower() == "csv":
+     caminho_saida = "output/processed_users.csv"
+     df.to_csv(caminho_saida, index=False)
+     print(f"{len(df)} registros salvos com sucesso.\nArquivo CSV gerado na pasta Output em {caminho_saida}.")
+    else:
+     raise ValueError(f"Formato de saída '{formato}' não suportado. Use 'json' ou 'csv'.")
 
 def main():
  try:
-    print("Iniciando o processo de ETL...")
-    ids, users = extract()
+    print("Iniciando o processo de ETL...\n(Se nenhum argumento for fornecido, o programa usará 'data/users.csv' como entrada e 'json' como formato de saída por padrão.)")
+    caminho = (f"data/{sys.argv[1]}") if len(sys.argv) > 1 else "data/users.csv"
+    print(f"Arquivo de entrada: {caminho}")
+    formato = sys.argv[2] if len(sys.argv) > 2 else "json"
+    print(f"Formato de saída: {formato}")
+    users = extract(caminho)
     print("Extração concluída.")
-    df = transform(ids, users)
+    df = transform(users)
     print("Transformação concluída.")
-    load(df)
-    print("Carregamento concluído. Processo de ETL finalizado.\nArquivo JSON gerado na pasta Output.")
+    load(df, formato)
+    print("Carregamento concluído. Processo de ETL finalizado.")
  except Exception as e:
     print(f"Ocorreu um erro durante o processo de ETL: {e}")
 
